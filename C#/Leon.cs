8ee11f59e946714671070447c97110acc4a1a0c7
AlexAlvarez172 <alex.v.valtazar04@gmail.com>
2026-04-17 04:30:45 -0600

    Add files via upload

---
+++ C#/Leon.cs
@@ -0,0 +1,12 @@
+﻿public class Leon : Animal
+{
+    public Leon(string nombre, int edad, double peso)
+        : base(nombre, "León", edad, peso)
+    {
+    }
+
+    public override void HacerSonido()
+    {
+        System.Console.WriteLine(Nombre + " ruge ");
+    }
+}
